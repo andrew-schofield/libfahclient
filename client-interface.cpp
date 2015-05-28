309bd3613be9b719eae2fb96782290afa499166f
Andrew Schofield <the.uncle.fungus@gmail.com>
2015-05-28 15:58:10 +0100

    Added code to print out client info in test application

--- client-interface.cpp
+++ client-interface.cpp
@@ -18,6 +18,45 @@
 
 #include "client-interface.h"
 
+std::stringstream ParseInfo(info info)
+{
+    std::stringstream out;
+    out << "**** Client ****" << std::endl
+        << "Website: " << info.client.Website << std::endl
+        << "Copyright: " << info.client.Copyright << std::endl
+        << "Author: " << info.client.Author << std::endl
+        << "Args: " << info.client.Args << std::endl
+        << "Config: " << info.client.Config << std::endl
+        << "**** Build ****" << std::endl
+        << "Version: " << info.build.Version << std::endl
+        << "Date: " << info.build.Date << std::endl
+        << "Time: " << info.build.Time << std::endl
+        << "SVN Revision: " << info.build.SVNRev << std::endl
+        << "Branch: " << info.build.Branch << std::endl
+        << "Compiler: " << info.build.Compiler << std::endl
+        << "Options: " << info.build.Options << std::endl
+        << "Platform: " << info.build.Platform << std::endl
+        << "Bits: " << info.build.Bits << std::endl
+        << "Mode: " << info.build.Mode << std::endl
+        << "**** System ****" << std::endl
+        << "OS: " << info.system.OS << std::endl
+        << "CPU: " << info.system.CPU << std::endl
+        << "CPUID: " << info.system.CPUID << std::endl
+        << "CPUs: " << info.system.CPUs << std::endl
+        << "Memory: " << info.system.Memory << std::endl
+        << "Free Memory: " << info.system.FreeMemory << std::endl
+        << "Threads: " << info.system.Threads << std::endl
+        << "GPUs: " << info.system.GPUs << std::endl
+        << "CUDA: " << info.system.CUDA << std::endl
+        << "CUDA Driver: " << info.system.CUDADriver << std::endl
+        << "On Battery: " << info.system.OnBattery << std::endl
+        << "UTC Offset: " << info.system.UTCoffset << std::endl
+        << "PID: " << info.system.PID << std::endl
+        << "CWD: " << info.system.CWD << std::endl
+        << "Windows Service: " << info.system.Win32Service << std::endl;
+    return out;
+}
+
 int main()
 {
     std::string hostname = "127.0.0.1";
@@ -39,6 +78,8 @@ int main()
     std::cout << "Authentication successful" << std::endl;
     std::cout << "Slot count: " << myfahclient->NumSlots() << std::endl;
     std::cout << "PPD: " << myfahclient->PPD() << std::endl;
+    std::cout << "Client Info:" << std::endl;
+    std::cout << ParseInfo(myfahclient->Info()).str() << std::endl;
     std::cout << "Closed connection to Folding@home command server" << std::endl;
     delete myfahclient;  
     getchar();
